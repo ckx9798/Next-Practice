fe0017f32a2a658424249b1e37c1e73804590013
ckx9798 <ckx12@Naver.com>
2024-09-23 16:54:48 +0900

    Feat Next 라우팅

---
+++ src/app/list/page.js
@@ -0,0 +1,13 @@
+export default function List() {
+  return (
+    <div className="main_container">
+      <h3> Products List</h3>
+      <div className="Product_List">
+        <h4>상품명 $40</h4>
+      </div>
+      <div className="Product_List">
+        <h4>상품명 $30</h4>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="mainContainer">
+      <h3>애플 후레시</h3>
+      <p>by dev Kim</p>
+    </div>
+  );
+}
